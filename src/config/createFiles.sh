b6aace3322d21bcd1cf45cfe1b9dde2f89fd8bf6
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-06 19:10:44 -0400

    feat(createFiles) .sh para gerar os arquivos que serao modificados posteriormente.

---
+++ src/config/createFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+mkdir ./config/vars
+touch ./config/vars/config.json
+touch ./config/.env
+touch ./config/vars/dev.packages.json
+touch ./config/vars/basic.packages.json
+
