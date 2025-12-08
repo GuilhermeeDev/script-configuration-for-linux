69a180150c65e185ed2e21473dc011d7c901ca25
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-08 16:06:11 -0400

    update (permissão de execução)

--- src/main.sh
+++ src/main.sh
@@ -7,8 +7,8 @@ done
 echo "================================================================================"
 echo "= Ola, bem vindo ao meu script de configuração para distros baseadas em Linux! ="
 echo "================================================================================"
-
 echo "Para contribuit com esse projeto acesse: https://github.com/GuilhermeeDev/script-configuration-for-linux.git"
 echo ""
+
 
 ./config/config.sh
