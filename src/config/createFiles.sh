bf4ee85706d2203d861be6a1e7e86c9c11776aac
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-10 18:26:16 -0400

    update (new files) Novos arquivos criados a partir .sh.

--- src/config/createFiles.sh
+++ src/config/createFiles.sh
@@ -3,9 +3,24 @@
 mkdir ./config/vars
 mkdir ./logs
 touch ./config/vars/config.json
-touch ./config/vars/basic-Packages.txt
-touch ./config/vars/dev-Packages.txt
 touch ./config/.env
+touch ./config/vars/basic-Packages.txt
+touch ./config/vars/games-Packages.txt
+touch ./config/vars/multimedia-Packages.txt
+touch ./config/vars/dev-Packages.txt
+touch ./config/vars/social-Packages.txt
+touch ./config/vars/dev-java-Packages.txt
+touch ./config/vars/dev-python-Packages.txt
+touch ./config/vars/dev-javascript-Packages.txt
+touch ./config/vars/dev-cpp-Packages.txt
+touch ./config/vars/dev-rust-Packages.txt
+touch ./config/vars/dev-go-Packages.txt
+touch ./config/vars/dev-php-Packages.txt
+touch ./config/vars/dev-ruby-Packages.txt
+touch ./config/vars/dev-elixir-Packages.txt
+touch ./config/vars/dev-devops-Packages.txt
+
+
 
 
 
